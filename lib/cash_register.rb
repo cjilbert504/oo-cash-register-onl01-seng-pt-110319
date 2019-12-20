a04e0753ae29f441fd224acc71b0f0b5dd2c9d81
Collin Jilbert <cjilbert504@gmail.com>
2019-12-20 05:52:23 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,14 +13,8 @@ attr_accessor :total, :discount, :last_transaction, :items
   def add_item(title, price, quantity = 1)
     @total += price * quantity
     @last_transaction = price * quantity
-    if quantity > 1
-   counter = 0
-   while counter < quantity
-     @items << title
-     counter += 1
-   end
-   else
-     @items << title
+    if quantity >= 1
+    @items << title
    end
   end
 
